e0d535f15cf6f012a0c8ed746619b2f43d7aee2d
Eleven <idiotalex@163.com>
2024-06-16 16:34:25 +0800

    feat: add playwright

--- src/eds-plus/main.py
+++ src/eds-plus/main.py
@@ -1,8 +1,19 @@
 import flet as ft
+from playwright.sync_api import sync_playwright
 
+
+def run(playwright):
+    chromium = playwright.chromium # or "firefox" or "webkit".
+    browser = chromium.launch(
+        headless = False,
+        slow_mo = 200
+    )
+    page = browser.new_page()
+    page.goto("http://eds.newtouch.cn/eds3/")
 
 def main(page: ft.Page):
     page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
-
+    with sync_playwright() as playwright:
+        run(playwright)
 
 ft.app(main)
